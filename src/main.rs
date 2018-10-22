dc1045ab0dba353f0cc1a91f2ed1b5744cdc05d5
Aaro Alhainen <aaro.alhainen@gmail.com>
2018-10-22 07:12:16 +0300

    Added initial test for read_file function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn read_file(fileuri: String) -> String {
+    return fileuri;
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let fileuri = "./src/test.txt".to_string();
+    let content = read_file(fileuri);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_file() {
+        let fileuri = "./src/text.txt";
+        let content = "test".to_string();
+        assert_eq!(read_file(fileuri.to_string()), content);
+    }
+
 }
