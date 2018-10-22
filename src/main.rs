84631a210093a0381a42673efec796d922e939b8
Aaro Alhainen <aaro.alhainen@gmail.com>
2018-10-22 07:19:52 +0300

    Added file reading

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
+
 
 fn read_file(fileuri: String) -> String {
-    return fileuri;
+    let path = Path::new(&fileuri);
+
+    // Try open file. If no file found return empty string.
+    let mut f = match File::open(path) {
+        Err(_why) => return String::new(),
+        Ok(f) => f,
+    };
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Something went wrong reading the file");
+
+    return contents;
 }
 
 
 fn main() {
     let fileuri = "./src/test.txt".to_string();
     let content = read_file(fileuri);
+    println!("{}", content);
 }
 
 #[cfg(test)]
@@ -18,9 +33,9 @@ mod tests {
 
     #[test]
     fn test_read_file() {
-        let fileuri = "./src/text.txt";
+        let fileuri = "./src/test.txt".to_string();
         let content = "test".to_string();
-        assert_eq!(read_file(fileuri.to_string()), content);
+        assert_eq!(read_file(fileuri), content);
     }
 
 }
